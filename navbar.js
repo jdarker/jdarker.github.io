bcf50d1b992f2226a6aa9d5aadb193ea4ec87c3e
jdarker <59739040+jdarker@users.noreply.github.com>
2021-04-23 15:30:45 +0100

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,13 @@
+/* When the user scrolls down, hide the nav. When the user scrolls up, show the nav */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
